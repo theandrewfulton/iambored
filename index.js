4434e7dfbbc6cfd452a3323fed3c29af89c6c2cb
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-06-13 21:06:48 +1000

    add headings and build out functionality

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const baseUrl = 'http://www.boredapi.com/api/'
 
-const apiData = document.getElementById("api-response")
+const activity = document.getElementById('activity')
+const responseContainer = document.getElementById('start-hidden')
+const accessibility = document.getElementById('accessibility')
+const category = document.getElementById('category')
+const participants = document.getElementById('participants')
+const price = document.getElementById('price')
+const link = document.getElementById('link')
 
-const changeColor = document.getElementById("color-paragraph");
-changeColor.style.color = 'red'
 
-const getRandomButton = document.getElementById("get-random-button")
+const getRandomButton = document.getElementById('get-random-button')
 getRandomButton.addEventListener('click', getRandomTask)
 
-
 async function getRandomTask() {
+    responseContainer.style.visibility = 'hidden'
     let data = null
     try {
-        apiData.innerText = "loading..."
+        activity.innerText = 'loading...'
          let response = await fetch(`${baseUrl}activity/`);
          data = await response.json()
-        apiData.innerText = data.activity;
+        activity.innerText = data.activity
+        accessibility.innerText = data.accessibility
+        category.innerText = data.type
+        participants.innerText = data.participants
+        price.innerText = data.price
+        // link.innerHTML = `<a href=${data.link}>${data.link}</>`
+        link.innerText = data.link
+        responseContainer.style.visibility = 'visible'
     } catch (error) {
-        apiData.innerText = "uh oh, something went wrong"
+        activity.innerText = 'uh oh, something went wrong'
     }
      
   }
+
